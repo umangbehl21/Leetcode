5e5ca3e0306e8a4b24ab806f24fbd037143374f2
Umang Behl <98390701+umangbehl21@users.noreply.github.com>
2023-05-17 13:58:38 +0530

    check binary tree is mirror of itself

---
+++ BinaryTrees/SymmetricTree.java
@@ -0,0 +1,40 @@
+package BinaryTrees;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetricHelp(root.left, root.right);
+    }
+
+    public boolean isSymmetricHelp(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val != right.val)
+            return false;
+
+        return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
+    }
+}
