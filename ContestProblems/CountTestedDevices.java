0d98403ec05193013ef655ad49bcadb0775f9a41
Umang Behl <98390701+umangbehl21@users.noreply.github.com>
2023-12-11 02:11:25 +0530

    leetcode 2960

---
+++ ContestProblems/CountTestedDevices.java
@@ -0,0 +1,27 @@
+package ContestProblems;
+
+public class CountTestedDevices {
+    class Solution {
+        public int countTestedDevices(int[] batteryPercentages) {
+            int count = 0;
+            for(int i = 0;i<batteryPercentages.length;i++)
+            {
+                if(batteryPercentages[i] > 0)
+                {
+                    count++;
+                    for(int j = i + 1;j<batteryPercentages.length;j++)
+                    {
+                        if(batteryPercentages[j] == 0) 
+                        {
+                            continue;
+                        }
+                        batteryPercentages[j] -= 1;
+                    }
+                }
+            }
+            return count;
+            
+            
+        }
+    }
+}
